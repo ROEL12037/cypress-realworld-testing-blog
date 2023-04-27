bf8108eada302eb804c56ea428192ddc9486262c
roel12037 <84542925+ROEL12037@users.noreply.github.com>
2023-04-27 17:26:00 -0500

    add answers to debug-failing-tests.cy.js

--- cypress/e2e/Practice/debug-failing-tests.cy.js
+++ cypress/e2e/Practice/debug-failing-tests.cy.js
@@ -3,28 +3,33 @@
   to find out why.
 */
 describe("Debug Failing Tests", () => {
-  beforeEach(() => {
-    cy.visit("http://localhost:3000");
-  });
+	beforeEach(() => {
+		cy.visit("http://localhost:3000");
+	});
 
-  it("displays the correct header text", () => {
-    cy.get('[data-test="home-header"]').contains("real world testing blog");
-  });
+	it("displays the correct header text", () => {
+		// cy.get('[data-test="home-header"]').contains("real world testing blog");
+		cy.get('[data-test="home-header"]').contains("Real World Testing Blog"); // .contains is case sensitive
+	});
 
-  it("the post links on the homepage link to the correct posts", () => {
-    cy.get('[data-test="post-link-0"]').click();
-    cy.location("pathname").should("eq", "/posts/pre-rendering");
-  });
+	it("the post links on the homepage link to the correct posts", () => {
+		cy.get('[data-test="post-link-0"]').click();
+		// cy.location("pathname").should("eq", "/posts/pre-rendering");
+		cy.location("pathname").should("eq", "/posts/ssg-ssr");
+	});
 
-  it("displays all of the posts on the homepage", () => {
-    cy.get('[data-test="posts-list"]').within(($post) => {
-      cy.get("a").should("have.length", 1);
-    });
-  });
+	it("displays all of the posts on the homepage", () => {
+		cy.get('[data-test="posts-list"]').within(($post) => {
+			// cy.get("a").should("have.length", 1);
+			cy.get("a").should("have.length", 2);
+		});
+	});
 
-  it("/api/posts returns a status code of 201", () => {
-    cy.request("GET", "http://localhost:3000/api/posts").then((response) => {
-      expect(response.status).to.eq(201);
-    });
-  });
+	// it("/api/posts returns a status code of 201", () => {
+	it("/api/posts returns a status code of 200", () => {
+		cy.request("GET", "http://localhost:3000/api/posts").then((response) => {
+			// expect(response.status).to.eq(201);
+			expect(response.status).to.eq(200);
+		});
+	});
 });
